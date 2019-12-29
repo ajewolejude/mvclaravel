1e35e1ee5a8f6d7aceab4b63d7e7858c544bc5c7
Ajewole Adedamola <adedamolaajewole@gmail.com>
2019-12-30 00:57:35 +0100

    update on models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,9 @@ class CreateUsersTable extends Migration
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
                 $table->rememberToken();
+                $table->string('first_name')->nullable();
+                $table->string('middle_name')->nullable();
+                $table->string('last_name')->nullable();
                 $table->string('city')->nullable();
                 $table->integer('role_id')->unsigned();
                 $table->timestamps();
